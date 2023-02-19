d3dd9cce9888956d56ec4d90ca42cb60eb91e65f
Vitalii Khanaliiev <41639784+Despeir993@users.noreply.github.com>
2023-02-19 11:52:11 +0200

    update task 10

---
+++ js/task-01.js
@@ -0,0 +1,34 @@
+const list = document.querySelectorAll('li.item');
+console.log(`Number of categories:`, list.length);
+
+const listItem = document.querySelectorAll('#categories>li');
+// console.log(listItem);
+
+listItem.forEach(el => {
+console.log(
+    `Category:
+     ${el.firstElementChild.textContent},
+      Elements:`, 
+    el.lastElementChild.children.length);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
